3095deefb58b261b7b658c6c3da0042b437edae9
Michael <kuharskiy10@gmail.com>
2022-11-11 16:21:49 +0200

    predeploy changes

--- src/App/App.js
+++ src/App/App.js
@@ -12,7 +12,7 @@ function App() {
 			<Router>
 				<Header/>
 				<Switch>
-					<Route exact path='/'>
+					<Route exact path='/main'>
 						<MainPage/>
 					</Route>
 					<Route path='/products'>
