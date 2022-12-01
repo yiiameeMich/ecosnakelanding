329b5fb1ad40942057712904fd3c0f61d082c3ae
Michael <kuharskiy10@gmail.com>
2022-12-01 12:06:46 +0200

    burger menu + adaptiveness

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.css'
 import {Link} from "react-router-dom";
 
 export default function Header() {
 
+	const [burgActive, setBurgActive] = useState('hamburger')
+	const [navActive, setNavActive] = useState('links-container')
+	const [headerActive, setHeaderActive] = useState('header-container')
+
+	const hamburger_overflow_changer = () => {
+		if (burgActive !== 'hamburger') {
+			document.body.style.overflow = ''
+		} else {
+			document.body.style.overflow = 'hidden'
+		}
+	}
+
 	return (
 		<div className='container'>
-			<div className='header-container'>
+			<div className={headerActive}>
 				<div className='logo-container'>
 					<Link to='/' className='header-logo main-text'> EcoSnake </Link>
 				</div>
-				<div className='links-container'>
+				<div className={navActive}>
 					<Link to='/products' className='header-link primary-text'> Shop All </Link>
-					<a className='header-link primary-text' href=''> Our Story </a>
-					<Link className='header-link primary-text' to='/blog'> Blog </Link>
-					<a className='header-link primary-text' href=''> Catalog </a>
+					<Link to='/blog' className='header-link primary-text'> Our Story </Link>
+					<Link to='/blog' className='header-link primary-text'> Blog </Link>
+					<Link to='/products' className='header-link primary-text'> Catalog </Link>
 				</div>
 
-				<div className='hamburger'>
+				<div className={burgActive} onClick={() => {
+					hamburger_overflow_changer()
+					setBurgActive((state) => state === 'hamburger' ? state += ' active' : state = 'hamburger')
+					setNavActive((state) => state === 'links-container' ? state += ' active' : state = 'links-container')
+					setHeaderActive((state) => state === 'header-container' ? state += ' active' : state = 'header-container')
+				}}>
 					<span className="bar">
 					</span>
 					<span className="bar">
